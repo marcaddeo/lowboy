eca430fcf715191fbb3886d9f2fc5e724b0e8667
Marc Addeo <marcaddeo@gmail.com>
2024-10-29 17:18:17 -0400

    Setup a basic axum server with askama templating

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use askama_axum::Template;
+use axum::{response::Html, routing::get, Router};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+async fn hello() -> HelloTemplate<'static> {
+    HelloTemplate { name: "world" }
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/hello", get(hello));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
