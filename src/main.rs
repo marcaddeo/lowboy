d9c2aa8b590cc270051f801ffcff50db0170b1bf
Marc Addeo <marcaddeo@gmail.com>
2024-10-31 21:13:19 -0400

    Add dummy posts

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,62 @@
 use askama::Template;
 use axum::{routing::get, Router};
+use fake::faker::company::en::CompanyName;
+use fake::faker::job::en::Title;
+use fake::faker::lorem::en::Paragraph;
+use fake::faker::name::en::{FirstName, LastName};
+use fake::{Dummy, Fake};
 use tower_http::services::ServeDir;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt as _};
 
-#[derive(Template)]
-#[template(path = "pages/home.html")]
-struct HomeTemplate<'a> {
-    title: &'a str,
-    name: &'a str,
+struct User {
+    first_name: String,
+    last_name: String,
+    byline: String,
+    avatar: String,
 }
 
-async fn index() -> HomeTemplate<'static> {
-    HomeTemplate {
-        title: "Default",
-        name: "World",
+impl User {
+    pub fn fake() -> Self {
+        let first_name: String = FirstName().fake();
+        let last_name: String = LastName().fake();
+
+        let byline = format!(
+            "{} - {}",
+            Title().fake::<String>(),
+            CompanyName().fake::<String>()
+        );
+
+        let avatar = format!(
+            "https://avatar.iran.liara.run/username?username={}+{}",
+            first_name, last_name
+        );
+
+        Self {
+            first_name,
+            last_name,
+            byline,
+            avatar,
+        }
     }
+}
+
+#[derive(Dummy)]
+struct Post {
+    #[dummy(expr = "User::fake()")]
+    author: User,
+    #[dummy(faker = "Paragraph(4..10)")]
+    content: String,
+}
+
+#[derive(Template)]
+#[template(path = "pages/home.html")]
+struct HomeTemplate {
+    posts: Vec<Post>,
+}
+
+async fn index() -> HomeTemplate {
+    let posts = fake::vec![Post; 3..8];
+    HomeTemplate { posts }
 }
 
 #[tokio::main]
