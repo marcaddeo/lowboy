22dd292889855582cf2dcf7570115fc66aadeb7b
Marc Addeo <marcaddeo@gmail.com>
2024-11-20 16:38:18 -0500

    Fix post model association

--- examples/demo/src/model/post.rs
+++ examples/demo/src/model/post.rs
@@ -9,7 +9,7 @@ use lowboy_record::prelude::*;
 #[apply(lowboy_record!)]
 #[derive(Debug, Default, Queryable, Identifiable, Selectable, Insertable, Associations)]
 #[diesel(table_name = crate::schema::post)]
-#[diesel(belongs_to(LowboyUserRecord, foreign_key = user_id))]
+#[diesel(belongs_to(UserRecord, foreign_key = user_id))]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Post {
     pub id: i32,
