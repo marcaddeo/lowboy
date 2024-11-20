f21c4fea1141c5e9ea793930ffdb1602939de027
Marc Addeo <marcaddeo@gmail.com>
2024-11-20 16:37:42 -0500

    Regenerate demo app schema

--- examples/demo/src/schema.rs
+++ examples/demo/src/schema.rs
@@ -1,10 +1,28 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    lowboy_user (id) {
+        id -> Integer,
+        username -> Text,
+        email -> Text,
+        password -> Nullable<Text>,
+        access_token -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     post (id) {
         id -> Integer,
         user_id -> Integer,
         content -> Text,
+    }
+}
+
+diesel::table! {
+    tower_sessions (id) {
+        id -> Text,
+        data -> Binary,
+        expiry_date -> Integer,
     }
 }
 
@@ -18,4 +36,12 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(post, user,);
+diesel::joinable!(post -> user (user_id));
+diesel::joinable!(user -> lowboy_user (lowboy_user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    lowboy_user,
+    post,
+    tower_sessions,
+    user,
+);
