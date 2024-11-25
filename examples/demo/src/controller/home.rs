df975987418c381600ff10ba5e15c9adcb5ff32b
Marc Addeo <marcaddeo@gmail.com>
2024-11-24 19:45:18 -0500

    Use lowboy error in home controller

--- examples/demo/src/controller/home.rs
+++ examples/demo/src/controller/home.rs
@@ -1,12 +1,14 @@
 use crate::{model::Post, view::Home};
 use axum::response::IntoResponse;
-use lowboy::{extract::DatabaseConnection, lowboy_view};
+use lowboy::{error::LowboyError, extract::DatabaseConnection, lowboy_view};
 
 #[axum::debug_handler]
-pub async fn home(DatabaseConnection(mut conn): DatabaseConnection) -> impl IntoResponse {
-    let posts = Post::list(&mut conn, Some(5)).await.unwrap();
+pub async fn home(
+    DatabaseConnection(mut conn): DatabaseConnection,
+) -> Result<impl IntoResponse, LowboyError> {
+    let posts = Post::list(&mut conn, Some(5)).await?;
 
-    lowboy_view!(Home { posts }, {
+    Ok(lowboy_view!(Home { posts }, {
         "title" => "Home",
-    })
+    }))
 }
