dcea3cac4bf326a097e17cdf28a4d55fe1ab4ec4
Marc Addeo <marcaddeo@gmail.com>
2024-11-29 19:18:52 -0500

    Add basic mailer support with lettre

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,4 +1,14 @@
+use serde::{Deserialize, Serialize};
+
+#[allow(dead_code)]
 type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub smtp_relay: String,
+    pub smtp_username: String,
+    pub smtp_password: String,
+}
